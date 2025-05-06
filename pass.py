841b96a659bad0d08958640abddd3a761a130399
Evil_Sensei <sajidmahmudsayan909@gmail.com>
2025-05-06 16:27:16 +0600

    Create pass.py

---
+++ pass.py
@@ -0,0 +1,12 @@
+n = int(input("Enter the num: "))
+
+if  n % 2 == 0:
+    print("Number is divisable by 2")
+if  n % 3 == 0:
+    pass
+if  n % 5 == 0:
+    print("Number is divisable by 5")
+if  n % 7 == 0:
+    pass
+else:
+    print("This number is not divisable by Inputted number")
